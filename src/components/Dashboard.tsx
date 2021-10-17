8f0592ed24e14b453ea5fc25d2a6593064e91364
Sebastian Hallqvist <31276987+sebboseb@users.noreply.github.com>
2021-10-17 17:36:04 +0200

    Changes

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,11 +5,9 @@ import { Link } from 'react-router-dom';
 import { db } from '../firebase';
 import { useAuth } from './contexts/AuthContext';
 import Navbar from './Navbar';
-// import Tilt from 'react-vanilla-tilt';
 import Tilt from 'react-parallax-tilt';
 
 function Dashboard() {
-
 
     const { currentUser } = useAuth();
     const [addCard, setAddCard] = useState(3);
@@ -32,8 +30,7 @@ function Dashboard() {
             const responseJson = await response.json();
 
             setSeries(responseJson.Search);
-
-            // console.log(series[0]);
+            console.log(series[0]);
         }
 
         getUsers();
@@ -45,58 +42,55 @@ function Dashboard() {
         console.log(clickedCard);
     }
 
+    function makeid(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * 
+     charactersLength));
+       }
+       return result;
+    }
+
     for (let i = 1; i <= addCard; i++) {
         items.push(
-
-            <Tilt scale={1.1}><li className="bg-black w-16 h-24 rounded mx-1 my-1 text-white" onClick={() => cardOnClick(series[i - 1])} ><img src={series[i - 1] && series[i - 1].Poster}></img></li></Tilt>
+            series[i - 1] && <Tilt className=" cursor-pointer" key={makeid(5)} tiltReverse={true} scale={1.05}><li className="bg-black w-16 h-24 rounded mx-1 my-1 text-white" onClick={() => cardOnClick(series[i - 1])} ><img src={series[i - 1].Poster}></img></li></Tilt>
             // {series.map((serie, index) => (<img src={serie[index + 1].Poster}></img>))}
             // series.map((serie, index) => (<li key={i} className="bg-black w-16 h-24 rounded mx-1 my-1 text-white"><img src={serie[index]}></img></li>))
             // series.map((serie, index) => (<img src={series[index].Poster}></img>))
         )
     }
-    //className="bg-black w-16 h-24 rounded mx-1 my-1 text-white"
+
     return (
         <>
             <Navbar></Navbar>
             <div className="absolute w-screen h-screen flex justify-center">
                 <div className="w-full h-full max-w-6xl bg-gray-700 flex flex-col items-center">
-
-
-
-
                     <div className=" mt-36 flex w-auto flex-col space-y-8 items-center">
-
                         <span><h1 className="text-white mt-16 font-semibold text-xl">Welcome back {username} here is what your friends have been watching</h1></span>
-
                         <div className="flex mt-16 space-x-4">
                             <Link to="/updateprofile" className=" rounded bg-white w-24 h-14">Update Profile</Link>
-
                             <button className="rounded bg-white w-24 h-14" onClick={() => setAddCard(addCard + 1)}>Add Card</button>
                             <button className="rounded bg-white w-24 h-14" onClick={() => setAddCard(addCard - 1)}>Remove Card</button>
-
                             <input type="text" placeholder="Name" onChange={(event) => { setNewName(event.target.value) }} />
                             <input type="number" placeholder="Mana" onChange={(event) => { setNewMana(event.target.value) }} />
-
                             <button className="rounded bg-white w-24 h-14" onClick={() => createUser()}>Firebase</button>
                         </div>
-
                         {addCard !== 0 ?
                             <div className=" w-3/4 bg-white max-w-xl rounded mt-16">
                                 <ul className="flex flex-wrap list-none pt-2 pb-2 justify-center">
                                     {items}
                                 </ul>
                             </div> : null}
-
-
-                        <div className=" w-3/4 bg-white max-w-xl rounded mt-16">
-                            <ul className="flex flex-wrap list-none pt-2 justify-center">
+                        {/* <div className=" w-3/4 bg-white max-w-xl rounded mt-16">
+                            <ul className="flex flex-wrap list-none pt-2 pb-2 justify-center">
                                 <img src={favourites.Poster} alt="" />
                             </ul>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
