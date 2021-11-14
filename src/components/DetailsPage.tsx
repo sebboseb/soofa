f8d347e1152d192ebebc48995155a198b730be66
Sebastian Hallqvist <31276987+sebboseb@users.noreply.github.com>
2021-11-14 23:04:25 +0100

    changes

--- src/components/DetailsPage.tsx
+++ src/components/DetailsPage.tsx
@@ -5,7 +5,7 @@ import StarRatings from 'react-star-ratings';
 import { Link, useParams } from 'react-router-dom';
 import { getCreditsRequest, getSeasonsRequest, getSearchRequest } from './utils/api';
 import { db } from '../firebase';
-import { doc, updateDoc, setDoc } from "firebase/firestore";
+import { doc, updateDoc, setDoc, onSnapshot } from "firebase/firestore";
 import { useAuth } from './contexts/AuthContext';
 import Tilt from 'react-parallax-tilt';
 import ProgressBar from "@ramonak/react-progress-bar";
@@ -27,6 +27,7 @@ function DetailsPage() {
     const [reviews, setReviews] = useState([]);
     const [reviewInput, setReviewInput] = useState(false);
     const [query, setQuery] = useState("");
+    const [postId, setPostId] = useState(makeid(9))
 
     useEffect(() => {
         const getUsers = async () => {
@@ -70,7 +71,6 @@ function DetailsPage() {
             console.log(snapshot.docs.map(doc => doc.data()));
             setReviews(snapshot.docs.map(doc => doc.data()));
 
-
             // db.collection("Posts").doc(id.replaceAll('-', ' ')).collection("userPosts").doc(currentUser.uid).get().then(doc => {
             //     if (doc.data()) {
             //         setReviews(doc.data());
@@ -93,9 +93,26 @@ function DetailsPage() {
         getSeriesRequest();
     }, [id, currentUser]);
 
+    function makeid(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() *
+                charactersLength));
+        }
+        return result;
+    }
+
     //getstars if namn (id) matchar firebase get star rating annars gör ny star rating
     const userDocumentFav = currentUser ? doc(db, "User", currentUser.uid, "Favourites", "Series") : null;
-    const reviewRef = currentUser ? doc(db, "Posts", id.replaceAll('-', ' '), "userPosts", currentUser.uid) : null;
+    const reviewRef = currentUser ? doc(db, "Posts", id.replaceAll('-', ' ')
+        //  + " " + makeid(9)
+        , "userPosts", currentUser.uid) : null;
+        const reviewRefMurloc = currentUser ? doc(db, "Posts", currentUser.uid) : null
+        //  + " " + makeid(9)
+        // , "userPosts", currentUser.uid) : null;
+    const postRef = currentUser ? doc(db, "Posts", currentUser.uid, "userPosts", (id.replaceAll('-', ' ') + " " + postId)) : null;
 
     async function addEpisode(murloc, starrating) {
         Object.assign(murloc, starrating);
@@ -124,11 +141,22 @@ function DetailsPage() {
         }
     }
 
-    async function addReview(reviewText, starrating) {
-        await setDoc(reviewRef, {
-            user: username,
-            review: reviewText,
-            starrating: starrating
+    async function addReview(reviewText, starrating, murloc) {
+        setPostId(makeid(9));
+        Object.assign(murloc, {review: reviewText}, {star_rating: starrating}, {user: username});
+        // await setDoc(reviewRef, {
+        //     user: username,
+        //     review: reviewText,
+        //     starrating: starrating,
+        //     // date: Timestamp.toDateTime().toString()
+        // });
+
+        await updateDoc(reviewRefMurloc, {
+            [murloc.name]: murloc,
+            // user: username,
+            // review: reviewText,
+            // starrating: starrating,
+            // date: Timestamp.toDateTime().toString()
         });
     }
 
@@ -137,7 +165,6 @@ function DetailsPage() {
     }
 
     for (let i = 1; i <= 20; i++) {
-
         seasonsLOL.push(
             season[i - 1] &&
             // (season[i - 1].name !== "Specials") ?
@@ -167,23 +194,25 @@ function DetailsPage() {
             <p className="text-white font-semibold">{id.replaceAll('-', ' ')}</p>
             <p className="text-white">{window.location.pathname}</p> */}
             <div className=" w-screen flex justify-center relative">
-                <div className=" w-full max-w-6xl min-h-screen h-auto bg-gray-700 flex">
+                <div className=" w-full max-w-6xl min-h-screen h-auto bg-letterboxd-bg flex">
                     <div className="flex flex-col items-center">
                         {inputClicked && <div className="absolute h-44 mt-0 z-50">
                             <div onClick={() => setInputClicked(false)} className="text-white font-semibold cursor-pointer">
                                 x</div>
                             <LoginPage />
                         </div>}
-                        <div className="w-3/4 relative">
-                            <div className=" bg-gradient-to-t via-transparent from-gray-700 h-full w-full absolute"></div>
-                            <div className=" bg-gradient-to- via-transparent from-gray-700 h-full w-full absolute"></div>
-                            <div className=" bg-gradient-to-l via-transparent from-gray-700 h-full w-full absolute"></div>
-                            <div className=" bg-gradient-to-r via-transparent from-gray-700 h-full w-full absolute"></div>
-                            <img src={`https://image.tmdb.org/t/p/original${succession.backdrop_path}`} alt={succession.name}></img>
+                        <div className="w-full relative -mt-16">
+                            <div className=" bg-gradient-to-t via-transparent from-letterboxd-bg h-full w-full absolute"></div>
+                            <div className=" bg-gradient-to- via-transparent from-letterboxd-bg h-full w-full absolute"></div>
+                            <div className=" bg-gradient-to-l via-transparent from-letterboxd-bg h-full w-full absolute"></div>
+                            <div className=" bg-gradient-to-r via-transparent from-letterboxd-bg h-full w-full absolute"></div>
+                            <div className=" overflow-hidden h-extra-height">
+                                <img src={`https://image.tmdb.org/t/p/original${succession.backdrop_path}`} alt={succession.name}></img>
+                            </div>
                         </div>
                         <div className="flex justify-between">
                             <div className="flex flex-col items-center">
-                                <img src={`https://image.tmdb.org/t/p/original${succession.poster_path}`} className="min-w-max max-w-min h-72 p-1" alt={succession.name}></img>
+                                <img src={`https://image.tmdb.org/t/p/original${succession.poster_path}`} className="min-w-max max-w-min h-80 rounded m-1 border-gray-50 border shadow" alt={succession.name}></img>
                                 {currentUser && <StarRatings
                                     rating={lolmurloc}
                                     starRatedColor="#f59e0b"
@@ -205,24 +234,11 @@ function DetailsPage() {
                                 {reviewInput ?
                                     <div>
                                         <input className=" absolute" type="text" value={query} onChange={onChange} />
-                                        <div onClick={() => addReview(query, lolmurloc)} className="bg-red-500 w-8 h-12 rounded shadow mt-2 cursor-pointer hover:bg-green-600 absolute">
+                                        <div onClick={() => addReview(query, lolmurloc, succession)} className="bg-red-500 w-8 h-12 rounded shadow mt-2 cursor-pointer hover:bg-green-600 absolute">
                                             <h1>Review</h1>
                                         </div>
                                     </div> : null}
-                                <Link to="/" className="font-semibold text-white text-xl">Dashboard</Link>
-                                <div>
-                                    {
-                                        castList.filter(Boolean).map((thingy, index) => (
-                                            <div className="flex flex-col items-center">
-                                                <Link to={{
-                                                    pathname: `/actor/${(thingy.name).replace(/\s/g, '-')}`,
-                                                }}>
-                                                    <h1 className="text-white font-semibold min-w-max">{thingy.name} - {thingy.character}</h1>
-                                                </Link>
-                                            </div>
-                                        ))
-                                    }
-                                </div>
+
                                 {/* <Timeline>
   <TimelineItem>
     <TimelineSeparator>
@@ -240,34 +256,52 @@ function DetailsPage() {
 </Timeline> */}
                             </div>
                             <div className="flex flex-col max-w-4xl">
-                                <h1 className=" text-gray-300 font-semibold p-4">{succession.name}</h1>
+                                <h1 className=" text-gray-300 font-semibold p-4 text-xl">{succession.name}</h1>
                                 <p className=" text-gray-300 font-semibold pb-4 pl-4 pr-4">{succession.overview}</p>
                                 <ProgressBar completed={60} className="w-44" />
-                                <div className="flex flex-col items-center">
-                                    <div className="flex flex-col items-center mt-44">
-                                        <h1 className="font-semibold text-xl text-gray-300">Säsonger</h1>
-                                        <ul className=" list-none flex max-w-5xl flex-wrap">
-                                            {seasonsLOL}
-                                        </ul>
-                                    </div>
+                                <div>
+                                    {
+                                        castList.filter(Boolean).map((thingy, index) => (
+                                            <div className="flex flex-col items-center">
+                                                <Link to={{
+                                                    pathname: `/actor/${(thingy.name).replace(/\s/g, '-')}`,
+                                                }}>
+                                                    <h1 className="text-white font-semibold min-w-max">{thingy.name} - {thingy.character}</h1>
+                                                </Link>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                                <div className="flex justify-center">
+                                    <ul className="bg-white max-w-xl w-screen rounded shadow mt-16 p-1">
+                                        {reviews.length !== 0 ?<div>
+                                            <div className="flex justify-end">
+                                                <Link to={{
+                                                    pathname: `/reviews/${id.replace(/\s/g, '-')}`,
+                                                }}>More Reviews</Link>
+                                            </div>
+                                            {
+                                                reviews.map((thingy) => (
+                                                    <Review user={thingy.user} review={thingy.review} stars={thingy.starrating}></Review>
+                                                ))}
+                                        </div>: <h1>Be the first one to leave a review!</h1>}
+                                    </ul>
 
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <div className="flex flex-col items-center">
+                                    {/* <h1 className="font-semibold text-xl text-gray-300">Seasons</h1> */}
+                                    <ul className=" list-none flex flex-col max-w-5xl flex-wrap">
+                                        {seasonsLOL}
+                                    </ul>
                                 </div>
 
                             </div>
+
                         </div>
 
-                        <div className="flex justify-center">
-                            <ul className="bg-white max-w-3xl w-screen rounded shadow mt-4 p-1">
-                            <div className="flex justify-end">
-                            <Link to={{
-                                                    pathname: `/${id.replace(/\s/g, '-')}/reviews`,
-                                                }}>More Reviews</Link>
-                            </div>
-                                {reviews.map((thingy) => (
-                                    <Review user={thingy.user} review={thingy.review} stars={thingy.starrating}></Review>
-                                ))}
-                            </ul>
-                        </div>
+
 
                     </div>
                 </div>
