fe67be0d9bc4b5ae52f8eb47d076f5a3a5aa7424
Sebastian Hallqvist <31276987+sebboseb@users.noreply.github.com>
2021-11-24 00:53:34 +0100

    changes

--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -5,7 +5,7 @@ import StarRatings from 'react-star-ratings';
 import { Link, useParams } from 'react-router-dom';
 import { getEpisodesRequest, getSeasonsRequest, getSearchRequest, getEpisodeRequest } from './utils/api';
 import { db } from '../firebase';
-import { doc, updateDoc, query, collection, where } from "firebase/firestore";
+import { doc, updateDoc, query, collection, where, setDoc } from "firebase/firestore";
 import { useAuth } from './contexts/AuthContext';
 
 function Episodes() {
@@ -19,14 +19,21 @@ function Episodes() {
     const [lolmurloc, setLolmurloc] = useState(0);
     const [isInFavourites, setIsInFavourites] = useState(false)
     // const [isInFavourites, setIsInFavourites] = useState(false);
+    const [postId, setPostId] = useState(makeid(9));
+    const [reviewInput, setReviewInput] = useState(false);
+    const [reviewQuery, setQuery] = useState("");
+    const [username, setUsername] = useState([]);
+    const [minusNumber, setMinusNumber] = useState(1);
 
     useEffect(() => {
-
         async function getSeriesRequest() {
             const seriesList = await getSearchRequest(id.replaceAll('-', ' '));
             const seasonList = await getSeasonsRequest(seriesList[0].id);
+            if (seasonList[0].name === "Specials") { setMinusNumber(0) }
             setSeason(seasonList);
             console.log(seasonList);
+
+
 
             const episodeList = await getEpisodesRequest(seriesList[0].id, seasonId);
             console.log(episodeList);
@@ -42,10 +49,10 @@ function Episodes() {
             currentUser && db.collection("User").doc(currentUser.uid).collection("Favourites").doc("Season").get().then(doc => {
                 if (doc.data()) {
                     setIsInFavourites(true);
-                    if (doc.data()[id.replaceAll('-', ' ') + " " + seasonList[seasonId - 1].name]) {
-                        if (doc.data()[id.replaceAll('-', ' ') + " " + seasonList[seasonId - 1].name]["star_rating"]) {
-                            console.log(doc.data()[id.replaceAll('-', ' ') + " " + seasonList[seasonId - 1].name]["star_rating"]);
-                            setLolmurloc(doc.data()[id.replaceAll('-', ' ') + " " + seasonList[seasonId - 1].name]["star_rating"]);
+                    if (doc.data()[id.replaceAll('-', ' ') + " " + seasonList[seasonId - minusNumber].name]) {
+                        if (doc.data()[id.replaceAll('-', ' ') + " " + seasonList[seasonId - minusNumber].name]["star_rating"]) {
+                            console.log(doc.data()[id.replaceAll('-', ' ') + " " + seasonList[seasonId - minusNumber].name]["star_rating"]);
+                            setLolmurloc(doc.data()[id.replaceAll('-', ' ') + " " + seasonList[seasonId - minusNumber].name]["star_rating"]);
                         }
                     }
                 }
@@ -55,10 +62,25 @@ function Episodes() {
                     console.log("lol");
                 }
             });
+
+            db.collection("User").doc(currentUser.uid).get().then(doc => {
+                setUsername(doc.data().Username);
+            });
         }
 
         getSeriesRequest();
     }, [currentUser, id, seasonId]);
+
+    function makeid(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() *
+                charactersLength));
+        }
+        return result;
+    }
 
     function changeRating(newRating, name) {
         setLolmurloc(newRating);
@@ -75,12 +97,42 @@ function Episodes() {
         });
     }
 
+    async function addReviewSeason(reviewText, starrating, murloc, seasonName) {
+        setPostId(makeid(9));
+        const reviewRefUpdated = doc(db, "Posts", "Reviews", "userPosts", id.replaceAll('-', ' '), "post", postId);
+        const reviewRefMurlocMrrrglUpdate = currentUser ? doc(db, "Posts", currentUser.uid, "userPosts", "Logs", "postSeason", postId) : null
+        let date = Date().toLocaleLowerCase();
+        Object.assign(murloc, { review: reviewText }, { star_rating: starrating }, { user: username }, { postId: postId }, { date: date }, {fullName: id.replaceAll('-', ' ') + " " + seasonName});
+
+
+        await setDoc(reviewRefUpdated, {
+            user: username,
+            review: reviewText,
+            starrating: starrating,
+            comments: { username: "Username" },
+            likes: 3,
+            reviewId: postId,
+            date: date,
+            // fullName: id.replaceAll('-', ' ') + " " + murlocSeason.name
+        });
+
+        await setDoc(reviewRefMurlocMrrrglUpdate, {
+            review: murloc
+        });
+    }
+
+    const onChange = (e) => {
+        e.preventDefault();
+
+        setQuery(e.target.value);
+    }
+
     return (
         <>
             <div className="w-screen bg-gray-800 text-white font-semibold">
                 <Link to="/">
                     {
-                        season[seasonId - 1] && season[seasonId - 1].name}
+                        season[seasonId - minusNumber] && season[seasonId - minusNumber].name}
                 </Link>
                 <div className="flex">
                     <div className="flex flex-col">
@@ -88,7 +140,7 @@ function Episodes() {
                             <Link to={{
                                 pathname: `/series/${id}`,
                             }}>
-                                <img className="w-96" src={`https://image.tmdb.org/t/p/original${season[seasonId - 1] && season[seasonId - 1].poster_path}`} alt="" />
+                                <img className="w-96 rounded" src={`https://image.tmdb.org/t/p/original${season[seasonId - minusNumber] && season[seasonId - minusNumber].poster_path}`} alt="" />
                             </Link>
                         </div>
                         <div className="p-1">
@@ -103,36 +155,48 @@ function Episodes() {
                                 starHoverColor="#f59e0b"
                             />}
                         </div>
-                        <button className=" w-32 h-12 bg bg-green-500 rounded shadow hover:bg-green-600 text-white font-semibold mt-2"
-                            onClick={() => addSeason(season[seasonId - 1], { star_rating: lolmurloc })}
-                        >Logga</button>
+                        <div className="flex space-x-1">
+                            <button className=" w-32 h-12 bg bg-green-500 rounded shadow hover:bg-green-600 dark:text-white font-semibold mt-2"
+                                onClick={() => addSeason(season[seasonId - minusNumber], { star_rating: lolmurloc })}
+                            >Logga</button>
+                            <div onClick={() => setReviewInput(!reviewInput)} className="bg-green-500 w-8 h-12 rounded shadow mt-2 cursor-pointer hover:bg-green-600">
+                            </div>
+                        </div>
+                        {reviewInput ?
+                            <div>
+                                <input className=" absolute text-black" type="text" value={reviewQuery} onChange={onChange} />
+                                <div onClick={() => addReviewSeason(reviewQuery, lolmurloc, season[seasonId - minusNumber], season[seasonId - minusNumber].name)} className="bg-red-500 w-8 h-12 rounded shadow mt-2 cursor-pointer hover:bg-green-600 absolute">
+                                    <h1>Review</h1>
+                                </div>
+                            </div> : null}
                     </div>
-                    <p>{season[seasonId - 1] && season[seasonId - 1].overview}</p>
+                    <p>{season[seasonId - minusNumber] && season[seasonId - minusNumber].overview}</p>
                 </div>
             </div>
+            {id} {seasonId}
             {/* <Link to="/" className="font-semibold text-white text-xl">Dashboard</Link>
             <p className="text-white font-semibold">{id.replaceAll('-', ' ')}</p>
             <p className="text-white">{window.location.pathname}</p> */}
-            <div className=" w-screen flex justify-center">
-                {id} {seasonId}
-                <ul className="flex flex-wrap">
+            <div className=" w-screen flex flex-wrap">
+               
+            
                     {
                         //onClick upp med meny kolla starrating
-                        episodes.filter(Boolean).map((thingy, index) => (
-
+                        episodes.map((thingy, index) => (
+<ul className="flex flex-wrap w-min">
                             <div className="flex flex-col items-center">
                                 <Link to={{
                                     pathname: `/${id}/season-${seasonId}/episode/${thingy.episode_number}`,
                                 }}>
-                                    <li className="bg-black w-52 rounded mx-1 my-1 text-white">
-                                        <img className="" src={`https://image.tmdb.org/t/p/original${thingy.still_path}`} alt={thingy.name}></img>
+                                    <li className="bg-black w-52 rounded mx-1 my-1 dark:text-white">
+                                        <img className="rounded" src={`https://image.tmdb.org/t/p/original${thingy.still_path}`} alt={thingy.name}></img>
                                     </li>
                                     <h1 className="text-white font-semibold">{thingy.name}</h1>
                                 </Link>
-                            </div>
+                            </div></ul>
                         ))
                     }
-                </ul>
+                
             </div>
         </>
     )
